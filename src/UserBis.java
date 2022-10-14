e1f7a337e8bd90faaf84c1d5b03d409728408079
andreabongiovanniuni <111112507+andreabongiovanniuni@users.noreply.github.com>
2022-10-14 19:39:39 +0200

    classi bis per fine didattico

---
+++ src/UserBis.java
@@ -0,0 +1,16 @@
+public class UserBis {
+    String username;
+
+    public UserBis(String username) {
+        this.username = username;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
