f61b9c650bbe94ee06a0f9b66729509c5dc099d6
andreabongiovanniuni <111112507+andreabongiovanniuni@users.noreply.github.com>
2022-10-18 08:52:54 +0200

    caspita ne so

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import UI.Page1;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Page1();
 
     }
 
--- src/UserBis.java
+++ src/UserBis.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.List;
 
 public class UserBis {
     String username;
